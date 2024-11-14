ea2c857dd13b5b42684cd27c9c3b29617b3e31ee
Talon Howe <talonh@umich.edu>
2024-11-14 10:52:45 -0500

    Completed lab16.cpp

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,13 @@
 // Code to implement the gcf function goes here
+int gcf(int a, int b) {
+    int r = 1;
+    while (r != 0) {
+        r = a % b;
+        if (r != 0) {
+            a = b;
+            b = r;
+        }
+    }
+    return(b);
+
+}
